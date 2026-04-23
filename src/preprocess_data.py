e8e472e2151dc999ccc55da06cfffdad6eca90e2
alia <alia@blackfoot.io>
2026-04-23 18:24:10 +0200

    Add an LSTM model

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+from tensorflow.keras import layers, models
 
 df = pd.read_csv("./data/raw/train_FD001.txt", sep=" ", header=None)
 
@@ -25,7 +27,8 @@ df = df.merge(rul, on="unit")
 df["RUL"] = df["max_cycle"] - df["cycle"]
 
 # clip RUL max value
-df["RUL"] = df["RUL"].clip(upper=125)
+clip_value = 125.0
+df["RUL"] = df["RUL"].clip(upper=clip_value)
 
 # set sequence_length
 sequence_length = 30
@@ -88,3 +91,53 @@ print("Test shape:", test_df.shape)
 X_train, y_train = create_sequences(train_df, sequence_length, features)
 X_val, y_val     = create_sequences(val_df, sequence_length, features)
 X_test, y_test   = create_sequences(test_df, sequence_length, features)
+
+y_train = y_train / clip_value
+y_val   = y_val / clip_value
+y_test  = y_test / clip_value
+
+# create model
+n_timesteps = X_train.shape[1]
+n_features = X_train.shape[2]
+
+model = models.Sequential([
+    layers.LSTM(32, input_shape=(n_timesteps, n_features), return_sequences=True),
+    layers.Dropout(0.3),
+
+    layers.LSTM(16),
+    layers.Dropout(0.3),
+
+    layers.Dense(16, activation="relu"),
+    layers.Dense(1)
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    loss="mse",
+    metrics=["mae"]
+)
+
+model.summary()
+
+# add early stopping
+callback = tf.keras.callbacks.EarlyStopping(
+    monitor="val_loss",
+    patience=3,
+    restore_best_weights=True
+)
+
+# train model using train and validation data
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_val, y_val),
+    epochs=50,
+    batch_size=64,
+    callbacks=callback,
+    verbose=1
+)
+
+# evaluate model using test data
+test_loss, test_mae = model.evaluate(X_test, y_test)
+
+print("Test MSE:", test_loss)
+print("Test MAE:", test_mae)
