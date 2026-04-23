046cec3e9c642decf954929e46d8a173b4042fa1
alia <alia@blackfoot.io>
2026-04-23 13:55:51 +0200

    delete computation from viz script

---
+++ src/vizualise_data.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("./data/raw/train_FD001.txt", sep=" ", header=None)
+
+# name columns
+columns = (
+    ["unit", "cycle"] +
+    [f"op_setting_{i}" for i in range(1, 4)] +
+    [f"sensor_{i}" for i in range(1, 24)]
+)
+
+df.columns = columns
+
+print("df description: \n", df.describe())
+
+# drop null sensors
+df.dropna(axis=1, inplace=True)
+# remaining not nan sensors number
+not_nan_sensors_nb = df.columns.size - 5
+
+# choose sensors to study from 1 to max sensors number
+sensors =  [f"sensor_{i}" for i in range(19, not_nan_sensors_nb)]
+
+# choose unit (engine) to study
+check_unit = 4
+unit = df[df["unit"] == check_unit]
+
+# Plot sensors evolution over time for specific unit(s)
+plt.figure(figsize=(10,6))
+for s in sensors:
+    plt.plot(unit["cycle"], unit[s], label=s)
+plt.xlabel("Cycle")
+plt.ylabel("Sensor values")
+plt.title(f"Sensor evolution - Unit {check_unit}")
+plt.legend()
+
+# Plot each given sensor mean evolution over time for all units
+for s in sensors:
+    plt.figure(figsize=(10,6))
+    df.groupby("cycle")[s].mean().plot()
+    plt.xlabel("Cycle")
+    plt.ylabel("Sensor mean value")
+    plt.title(f"{s} mean evolution for all Units")
+    plt.legend()
+plt.show()
